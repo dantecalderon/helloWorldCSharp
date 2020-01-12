8847b6d912b3eb0c773a9c9d336eb6b2c2900b9c
Dante Calderon <mail@dantecalderon.dev>
2020-01-12 03:24:35 -0500

    My first hello world after a long time in C# and I love itk

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace helloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "Dante Calderon";
+            Console.WriteLine("Hola amigo " + nombre);
+        }
+    }
+}
